9c15fdf14f4cc5effddad28cd744c359fc9b7854
mirelameic <mirelameic4@gmail.com>
2022-07-19 02:40:09 -0300

    add maven

---
+++ src/main/java/com/museubd/Main.java
@@ -0,0 +1,34 @@
+package com.museubd;
+import java.sql.Date;
+import java.util.ArrayList;
+import java.util.List;
+import com.museubd.model.bean.Artista;
+import com.museubd.model.dao.ArtistaDAO;
+
+public class Main{
+    public static void main(String[] args){
+
+        /////////////////////////TESTES ARTISTA/////////////////////////
+        Artista artista = new Artista();
+        ArtistaDAO artistaDAO = new ArtistaDAO();
+
+        artista.setNome("teste");
+        artista.setDescricao("descricao");
+        artista.setEstiloPrincipal("estiloPrincipal");
+        artista.setPeriodoArt("periodoArt");
+        artista.setPaisOrig("paisOrigem");
+        artista.setDataNasc(Date.valueOf("2015-03-31"));
+        artista.setDataMorte(Date.valueOf("2015-03-31"));
+
+        artistaDAO.insert(artista);
+        artista.setDescricao("nova descricao");
+        artistaDAO.update(artista);
+        artistaDAO.delete(artista);
+
+        List<Artista> artistasList = new ArrayList<>();
+        artistasList = artistaDAO.read();
+        for(int i = 0; i<artistasList.size(); i++){
+            System.out.println(artistasList.get(i).getNome());
+        }
+    }
+}
